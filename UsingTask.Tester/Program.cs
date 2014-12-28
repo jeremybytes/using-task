6e5f2d1a2aa1fad0fcb0ef0bc30a8a3e98860a01
jeremybytes <jeremy@jeremybytes.com>
2014-12-28 08:27:25 -0800

    Added 'ContinueWith' to console application

--- UsingTask.Tester/Program.cs
+++ UsingTask.Tester/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using UsingTask.Library;
 using UsingTask.Shared;
 
@@ -10,10 +11,18 @@ namespace UsingTask.Tester
         static void Main(string[] args)
         {
             var repository = new PersonRepository();
-            List<Person> people = repository.Get().Result;
+            Task<List<Person>> people = repository.Get();
+            people.ContinueWith(FillConsole);
+            for (int i = 0; i < 5; i++)
+                Console.WriteLine(i);
+            Console.ReadLine();
+        }
+
+        private static void FillConsole(Task<List<Person>> peopleTask)
+        {
+            List<Person> people = peopleTask.Result;
             foreach (var person in people)
                 Console.WriteLine(person.ToString());
-            Console.ReadLine();
         }
     }
 }
